9221df80c2cd46cf280845424fe539d15946ea83
Juan Jose <chambijuanjose05@gmail.com>
2023-10-02 21:17:08 -0300

    useHooks

--- src/Components/ContadorRed.tsx
+++ src/Components/ContadorRed.tsx
@@ -1,8 +1,45 @@
+import { useReducer } from "react"
+const initialState = {
+  contador : 0
+}
+
+type ActionType = 
+  | {type: "incrementar"}
+  | {type : "decrementar"}
+  | {type : "custom", payload:number}
+
+function contadorReducer (state: typeof initialState, action:ActionType) {
+  switch (action.type) {
+    case "incrementar":
+      return {
+        ...state, 
+        contador:state.contador + 1
+      }
+    case "decrementar":
+      return {
+        ...state,
+        contador : state.contador - 1
+      }
+    case "custom" : 
+      return {
+      ...state,
+      contador: state.contador = action.payload
+    }
+    default:
+      return state
+  }
+
+} 
 
 function ContadorRed() {
+  const [contadorState, dispatch] = useReducer(contadorReducer, initialState)
+
   return (
     <div>
-        
+        <h3>Contador: {contadorState.contador}</h3>
+        <button onClick={() => dispatch({type:"incrementar"})} className="btn btn-outline-primary">+1</button>
+        <button onClick={() => dispatch({type:"decrementar"})} className="btn btn-outline-primary">-1</button>
+        <button onClick={() => dispatch({type:"custom", payload: 100})} className="btn btn-outline-danger">100</button>
     </div>
   )
 }
--- src/Components/TimerPadre.tsx
+++ src/Components/TimerPadre.tsx
@@ -6,7 +6,7 @@ function TimerPadre() {
 
   return (
     <div>
-        <span>Milisegundo {milisegundos}</span>
+        <span>El tiempo avanza cada {milisegundos} milisegundos = {milisegundos === 1000 ? 1 :(milisegundos === 2000 ? 2 : 3)} segundos</span>
         <br />
         <button onClick={() => setMilisegundos(1000)} className="btn btn-outline-success ">1 segundo</button>
         <button onClick={() => setMilisegundos(2000)} className="btn btn-outline-success ">2 segundos</button>
